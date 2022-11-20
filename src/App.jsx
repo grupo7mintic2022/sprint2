c657decfa576c0276989c2cfba7baee8bdcd5e02
OFITE - JHONATAN RENALS CUADRADO <JHONATAN.RENALS@policia.gov.co>
2022-11-20 17:32:41 -0500

    plantillas iniciales del proyecto sin conexion al back

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+//import './css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return 
+}
+
+export default App
